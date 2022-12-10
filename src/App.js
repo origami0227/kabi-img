2eddacd918569b3d183da59e3bebc9052c27285b
origami <1071242996@qq.com>
2022-12-10 15:15:34 +0800

    feature:实现分包懒加载

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import Home from "./pages/Home";
-import History from "./pages/History";
-import About from "./pages/About";
 import {Routes, Route} from 'react-router-dom'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import React, {Suspense, lazy} from 'react';
+import Loading from "./components/Loading";
+// import Home from "./pages/Home";
+// import History from "./pages/History";
+// import About from "./pages/About";
+//对这三个界面组件进行懒加载
+const Home = lazy(() => import('./pages/Home'))
+const History = lazy(() => import('./pages/History'))
+const About = lazy(() => import('./pages/About'))
 
 function App() {
     return (
         <div className="app">
             <Header/>
+            <Suspense fallback={<Loading/>}>
+                {/*fallback 方法用于组件没有加载完成时页面的显示*/}
             <Routes>
                 {/*exact是精准匹配，只有路径完全一致才能被匹配到*/}
                 {/*v6版本中用element代替了component/render*/}
@@ -16,6 +24,7 @@ function App() {
                 <Route path='/history' element={<History/>}/>
                 <Route path='/about' element={<About/>}/>
             </Routes>
+            </Suspense>
             <Footer/>
         </div>
     );
