c3d718c8b33a90871b2ad8f1701bebefa56f6338
origami <1071242996@qq.com>
2022-12-13 12:18:13 +0800

    feature:引入antd并且实现基本登录注销效果

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from './logo.svg'
 import {NavLink} from "react-router-dom";
 import styled from 'styled-components'
+import {Button} from "antd";
 
 const Header = styled.header` //返回一个新的标签
   background-color: #02101f;
@@ -17,12 +18,22 @@ const Logo = styled.img`
 const StyledLink = styled(NavLink)`
   color: #fff;
   margin-left: 30px;
+
   &.active {
     border-bottom: 1px solid #ccc;
   }
 `
+const Login = styled.div`
+  margin-left: auto;
+`
+const StyledButton = styled(Button)`
+  margin-left: 10px;
+`
+
 
 function Component() {
+    //状态判断
+    const [isLogin, setIsLogin] = useState(false) //默认是false
     return (
         <Header>
             <Logo src={logo} alt=''/>
@@ -31,8 +42,26 @@ function Component() {
                 <StyledLink to='/history' activeclassname='active'>上传历史</StyledLink>
                 <StyledLink to='/about' activeclassname='active'>关于我</StyledLink>
             </nav>
+            <Login>
+                {/*判断登录状态*/}
+                {/*如果登录了就展示用户名否则展示登录/注册按钮*/}
+                {isLogin ? <>
+                        卡比兽 <StyledButton type="primary" onClick={()=>setIsLogin(false)}>注销</StyledButton>
+                    </> :
+                    <>
+                        <StyledButton type="primary" onClick={()=>setIsLogin(true)}>
+                            <NavLink to="/login">登录</NavLink>
+                        </StyledButton>
+                        <StyledButton type="primary">
+                            <NavLink to='/register'>注册</NavLink>
+                        </StyledButton>
+                    </>
+                }
+            </Login>
         </Header>
     )
 }
 
 export default Component
+
+
