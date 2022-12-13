769ee2cc0bb98dbb166a24641d6657d06634f227
origami <1071242996@qq.com>
2022-12-13 13:41:55 +0800

    feature:引入form完成注册页面样式

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,15 +1,94 @@
-import React from "react";
-import {observer} from "mobx-react";
-import {useStores} from "../stores";
+import React from 'react';
+import {Button, Checkbox, Form, Input} from 'antd';
+import styled from 'styled-components'
 
-const Register = observer(() => {
-    const {AuthStore} = useStores() //获取AuthStore
+const Wrapper = styled.div`
+  max-width: 600px;
+  margin: 30px auto;
+  box-shadow: 2px 2px 4px 0 rgba(0,0,0,0.2);
+  padding: 20px;
+`
+const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 30px;
+`
 
+const Register = () => {
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
     return (
-        <>
-            <h1>Register:{AuthStore.values.username}</h1>
-        </>
-    )
-})
+        <Wrapper>
+            <Title>注册</Title>
+            <Form
+                name="basic"
+                labelCol={{
+                    span: 8,
+                }}
+                wrapperCol={{
+                    span: 16,
+                }}
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="用户名"
+                    name="username"
+                    rules={[
+                        {
+                            required: true,
+                            message: '请输入用户名',
+                        },
+                    ]}
+                >
+                    <Input/>
+                </Form.Item>
 
-export default Register
+                <Form.Item
+                    label="密码"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: '请输入密码',
+                        },
+                    ]}
+                >
+                    <Input.Password/>
+                </Form.Item>
+
+                <Form.Item
+                    label="确认密码"
+                    name="confirmPassword"
+                    rules={[
+                        {
+                            required: true,
+                            message: '请再次确认密码',
+                        },
+                    ]}
+                >
+                    <Input.Password/>
+                </Form.Item>
+
+                <Form.Item
+                    wrapperCol={{
+                        offset: 8,//偏移
+                        span: 16,
+                    }}
+                >
+                    <Button type="primary" htmlType="submit">
+                        提交
+                    </Button>
+                </Form.Item>
+            </Form>
+        </Wrapper>
+    );
+};
+export default Register;
