bee02d2cfcedfaaa43db09a3154dfcea039cc16d
origami <1071242996@qq.com>
2022-12-17 16:03:26 +0800

    feature:引入antd拖拽上传组件实现点击和拖拽上传

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,18 +1,18 @@
-import React, {useRef} from 'react'
+import React from 'react'
 import {useStores} from "../stores";
 import {observer} from 'mobx-react'
+import {Upload} from 'antd';
+import {InboxOutlined} from '@ant-design/icons';
 
+const {Dragger} = Upload;
 const Uploader = observer(() => {
-    const ref = useRef() //标记
     const {ImageStore} = useStores() //使用ImageStore
-    const bindChange = () => {
-        //ref.current得到刚才上传的文件以及对应的文件名
-        window.file = ref.current //便于调试
-        console.log(ref.current)
-        if (ref.current.files.length > 0) {
+    const props = {
+        showUploadList:false, //隐藏文件展示列表
+        beforeUpload: file => {
             //里面有文件的情况（即已经上传）即可调用上传逻辑
-            ImageStore.setFile(ref.current.files[0]) //把上传的文件设置进去
-            ImageStore.setFilename(ref.current.files[0].name)//设置文件名
+            ImageStore.setFile(file) //把上传的文件设置进去
+            ImageStore.setFilename(file.name)//设置文件名
             //上传
             ImageStore.upload()
                 .then((serverFile) => {
@@ -22,13 +22,31 @@ const Uploader = observer(() => {
                 console.log('上传失败')
                 console.log(err)
             })
+            //取消自动上传
+            return false
         }
     }
 
     return (
         <div>
             <h1>文件上传</h1>
-            <input type="file" ref={ref} onChange={bindChange}/>
+            <Dragger {...props}>
+                <p className="ant-upload-drag-icon">
+                    <InboxOutlined/>
+                </p>
+                <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                <p className="ant-upload-hint">
+                    Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+                    band files
+                </p>
+            </Dragger>
+            <h1>上传结果:</h1>
+            {ImageStore.serverFile
+                ?
+                <div>{ImageStore.serverFile.attributes.url.attributes.url}</div>
+                :
+                null
+            }
         </div>
     )
 })
