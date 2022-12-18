7516711e535cf3e841570d7a671a2545622ab57b
origami <1071242996@qq.com>
2022-12-18 13:48:41 +0800

    wip:准备制作Tips组件

---
+++ src/components/Tips.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Tips = () => {
+    return (
+        <div>
+            Tips
+        </div>
+    )
+}
+
+export default Tips
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,14 @@ import React from "react";
 import {observer} from 'mobx-react'
 import {useStores} from '../stores'
 import Uploader from "../components/Uploader";
+import Tips from "../components/Tips";
 
 const Home = observer(() => {
     const {UserStore} = useStores()
     return (
         <>
             <div>{
-                UserStore.currentUser ? <>
-                        Hello,{UserStore.currentUser.attributes.username}
-                    </>
-                    :
-                    "该用户未登录"
+                UserStore.currentUser ? null : <Tips/>
             }</div>
             <Uploader/>
         </>
