9cf19192e86cc3f70354d4257d8db06da231dba1
origami <1071242996@qq.com>
2022-12-23 11:47:25 +0800

    error:无法显示数据

--- src/components/List.js
+++ src/components/List.js
@@ -11,25 +11,38 @@ const DataList = observer(() => {
         HistoryStore.find()
     }
     //InfiniteScroll的属性写成一个对象
-    const options = {
-        initialLoad: true,
-        pageStart:0,
-        loadMore,
-        hasMore:!HistoryStore.isLoading && HistoryStore.hasMore,//加载完毕且有hasMore
-        useWindow:true,
-        dataLength:HistoryStore.list.length
-    }
+    // const options = {
+    //     initialLoad: true,
+    //     pageStart: 0,
+    //     loadMore,
+    //     hasMore: !HistoryStore.isLoading && HistoryStore.hasMore,//加载完毕且有hasMore
+    //     useWindow: true,
+    //     dataLength: HistoryStore.limit
+    // }
     return (
         <InfiniteScroll
-            {...options}
+            initialLoad={true}
+            pageStart={0}
+            loadMore={loadMore}
+            hasMore={!HistoryStore.isLoading && HistoryStore.hasMore}
+            useWindow={true}
+            dataLength={HistoryStore.limit}
         >
             <List dataSource={HistoryStore.list}
                   renderItem={
-                      item => (
+                      item =>
                           //使用key是diff时可以提高效率
                           <List.Item key={item.id}>
+                              <div>
+                                  <img src={item.attributes.url.attributes.url} style={{height: '100px'}} alt=""/>
+                              </div>
+                              <div>
+                                  <h5>{item.attributes.filename}</h5>
+                              </div>
+                              <div>
+                                  <a href={item.attributes.url.attributes.url}>{item.attributes.url.attributes.url}</a>
+                              </div>
                           </List.Item>
-                      )
                   }
             >
                 {HistoryStore.isLoading && HistoryStore.hasMore && (
