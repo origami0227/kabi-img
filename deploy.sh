9ee613e72c438b5353b411bfa243313806d2eecc
origami <1071242996@qq.com>
2022-12-25 12:32:52 +0800

    更新自动部署脚本

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m 'update' &&
+git branch -M main  &&
+git remote add origin git@github.com:origami0227/kabi-img-website.git &&
+git push -f -u origin main &&
+cd ..
+echo https://origami0227.github.io/kabi-img-website/index.html
